2cff4f01f188f996befc3f287ee5f7fddbf8661c
mckinleysasha <134669703+mckinleysasha@users.noreply.github.com>
2024-05-09 19:59:45 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+let paris = {
+  name: "Paris",
+  country: "France",
+  language: "French",
+  temperature: 18,
+  capitalCity: true
+};
+
+let sydney = {
+  name: "Sydney"
+  country: "Australia"
+  language: "english"
+  temperature: "78"
+  capitalCity: "true"
+}
+
+let australianCities = { "Melbourne","Perth", "Adelaide"};
+
+let cities = {
+  name: "Seattle"
+  country: "USA"
+  language: "english"
+  temperature: "69"
+  capitalCity: "false"
+}
+
+let cities = {
+  name: "Athens"
+  country: "Greece"
+  language: "Greek"
+  temperature: "79"
+  capitalCity: "true"
+}
+let cities = {
+  name: "Seoul"
+  country: "Korea"
+  language: "Korean"
+  temperature: "69"
+  capitalCity: "true"
+}
+
+console.log(paris.temperature);
+console.log(sydney.temperature);
+console.log(australianCities[0]);
+console.log(australianCities[1]);
+console.log(australianCities[2]);
+console.log(cities[0].name);
+console.log(cities[0].temperature);
+
+console.log(cities[1].name);
+console.log(cities[1].temperature);
+
+console.log(cities[2].name);
+console.log(cities[2].temperature);
+
